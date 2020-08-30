128de24e144e74d10c15fdb7a49bf446eb9c412b
Arpit Jain <arpitjain1094@gmail.com>
2020-08-30 17:42:06 +0530

    Update api key (#2)
    * Propagate api_key changes from upstream geocoder
    
    Ref: https://github.com/alexreisner/geocoder/commit/773ac0fb67ffc18172573e9c5b052126276e01c1
    
    * Typo -> app_key to api_key
    
    Co-authored-by: Evan Wechsler <ewechsler@gocloudlogistics.com>

--- lib/geocoder/lookups/concerns/here_lookup_default_methods.rb
+++ lib/geocoder/lookups/concerns/here_lookup_default_methods.rb
@@ -7,37 +7,24 @@ module Geocoder::Lookup
     end
 
     def required_api_key_parts
-      ["app_id", "app_code"]
+      ["api_key"]
+    end
+
+    def supported_protocols
+      [:https]
     end
 
     private
 
     def query_url_here_options(query)
       options = {
-        app_id: api_key,
-        app_code: api_code,
+        apikey: configuration.api_key,
         language: (query.language || configuration.language)
       }
     end
 
-    def api_key
-      if (a = configuration.api_key)
-        return a.first if a.is_a?(Array)
-      end
-    end
-
-    def api_code
-      if (a = configuration.api_key)
-        return a.last if a.is_a?(Array)
-      end
-    end
-
     def domain
-      if configuration.cit
-        'cit.api.here.com'
-      else
-        'api.here.com'
-      end
+      'ls.hereapi.com'
     end
   end
 end
